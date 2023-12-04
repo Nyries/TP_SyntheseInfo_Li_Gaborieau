f00ec89642f4cde032c90c456549f2530b2d4026
Nyries <cyprien.gaborieau@gmail.com>
2023-12-04 10:57:48 +0100

    Q6 Commit

---
+++ Q6.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include "Q3.h"
+#include "Q1.h"
+
+char linestart6[256]= "enseash % ";
+
+
+void executeCommand6(char** command){
+    pid_t pid;
+    pid =fork();
+    struct timespec time_start, time_stop;
+    int status;
+    clock_gettime(CLOCK_REALTIME, &time_start);
+    if (pid==-1){
+        perror("Fork impossible");
+    }
+    else if (pid==0){
+        execvp(command[0], command);
+        exit(EXIT_SUCCESS);
+    }
+    else{
+        while ((pid=wait(&status))!=-1){
+            clock_gettime(CLOCK_REALTIME, &time_stop);
+            unsigned long long int duration = time_stop.tv_nsec-time_start.tv_nsec;
+            if (WIFEXITED(status)) {
+                sprintf( linestart6,"enseash [exit:%d|%lldms] %% ", WEXITSTATUS(status),duration/1000000);
+            } else if (WIFSIGNALED(status)) {
+                sprintf(linestart6,"enseash [sign:%d|%lldms] %% ", WTERMSIG(status), duration/1000000);
+            }
+            write(STDOUT_FILENO,linestart6, strlen(linestart6));
+        }
+    }
+}
+int mainQuestion6(){
+    welcome();
+    char command[256],*argv[100];
+    ssize_t number;
+    int i=1;
+    write(STDOUT_FILENO,linestart6, strlen(linestart6));
+    while((number = read(STDIN_FILENO, command, sizeof(command)))>0) {
+        if (number != 0) {
+            command[number - 1] = '\0';
+        }
+        argv[0] = strtok(command, " "); //helps us to detect spaces, so we can detect arguments in commands.
+        while((argv[i] = strtok(NULL, " "))!=NULL) {//We could also try to detect '-' to find arguments.
+            i++;
+        }
+        if (strcmp(command, "exit") == 0 || number == 0) {
+            exitFunction();
+        }
+        executeCommand6(argv);
+    }
+    if (number ==-1){
+        perror("read");
+    }
+    exit(EXIT_SUCCESS);
+}
--- main.c
+++ main.c
@@ -3,23 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-/*#include "Q1.h"
-#include "Q2.h"
-#include "Q3.h"*/
-//#include "Q4.h"*/
-#include "Q5.h"
+//#include "Q1.h"
+//#include "Q2.h"
+//#include "Q3.h"
+//#include "Q4.h"
+//#include "Q5.h"
+#include "Q6.h"
 
 void main(){
 
-    /* Code for question 1
-    welcome();
-    prompt();*/
-    /* Code for question 2
-    mainQuestion2();*/
-    /* Code for question 3
-    mainQuestion3();*/
-    /* Code for question 4
-    mainQuestion4();*/
+    // Code for question 1
+    //welcome();
+    //prompt();
+    // Code for question 2
+    //mainQuestion2();
+    // Code for question 3
+    //mainQuestion3();
+    // Code for question 4
+    //mainQuestion4();
     //Code for question 5
-    mainQuestion5();
+    //mainQuestion5();
+    // Code for question 6
+    mainQuestion6();
 }
