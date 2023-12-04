3d652a914315d61a535029faefb5be40c8a57402
llicecream <lucieshli@gmail.com>
2023-12-04 10:57:12 +0100

    commentaires

--- Q4.c
+++ Q4.c
@@ -16,16 +16,16 @@ void executeCommand4(char* command){
     if (pid==-1){
         perror("Fork impossible");
     }
-    else if (pid==0){
+    else if (pid==0){  //child
         execlp(command, command, NULL);
         exit(EXIT_SUCCESS);
     }
-    else{
+    else{ //father
         while((pid=wait(&status))!=-1) {
             if (WIFEXITED(status)) {
-                sprintf(linestart4, "enseash [exit:%d] %% ", WEXITSTATUS(status));
+                sprintf(linestart4, "enseash [exit:%d] %% ", WEXITSTATUS(status));  // If the child process exited normally, display the exit code in the prompt
             } else if (WIFSIGNALED(status)) {
-                sprintf(linestart4, "enseash [sign:%d] %% ", WTERMSIG(status));
+                sprintf(linestart4, "enseash [sign:%d] %% ", WTERMSIG(status));  // If the child process was terminated by a signal, display the signal number in the prompt
             }
         }
     }
@@ -38,9 +38,9 @@ int mainQuestion4(){
     while((number = read(STDIN_FILENO, command, sizeof(command)))>0) {
         write(STDOUT_FILENO,linestart4, strlen(linestart4));
         if (number != 0) {
-            command[number - 1] = '\0';
+            command[number - 1] = '\0'; // Remove newline character from user input
         }
-        if (strcmp(command, "exit") == 0 || number == 0) {
+        if (strcmp(command, "exit") == 0 || number == 0) {   // Check for "exit" command to terminate the shell
             exitFunction();
         }
         executeCommand4(command);
@@ -51,3 +51,4 @@ int mainQuestion4(){
 
     exit(EXIT_SUCCESS);
 }
+
